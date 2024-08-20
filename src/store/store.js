70247aee5529761cc56c8ddaf03c96554d54f225
ujjwal <ujjwal.12100544@lpu.in>
2024-08-20 22:57:09 +0530

    little updates

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authslice";
-import testReducer from "./slices/testSlice";
-import submissionReducer from "./slices/submissionSlice";
 import storage from "redux-persist/lib/storage";
 import {persistReducer,persistStore} from "redux-persist";
 const persistConfig = {
@@ -12,8 +10,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
-    test: testReducer,
-    submission: submissionReducer,
+    
   },
 });
 export const persistor = persistStore(store);
